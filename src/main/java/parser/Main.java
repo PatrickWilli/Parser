89e6f5f8e8331191586e3b945dc946f4a2e6bfc0
phamm <phamm@192.168.1.101>
2017-04-16 14:12:37 +0200

    Added org.json dependency in pom.xml

--- src/main/java/parser/Main.java
+++ src/main/java/parser/Main.java
@@ -5,12 +5,12 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-		XMLReader xmlreader = new XMLReader("C:\\Users\\phamm\\Desktop\\xmltoparse.xml");
-		XMLtoJSON xtoj = new XMLtoJSON(xmlreader.getXMLDocumentAsString());
-		System.out.println(xtoj.xmltojson());
+            XMLReader xmlreader = new XMLReader("C:\\Users\\phamm\\Desktop\\xmltoparse.xml");
+            XMLtoJSON xtoj = new XMLtoJSON(xmlreader.getXMLDocumentAsString());
+            System.out.println(xtoj.xmltojson());
 		
-		/*JSONReader jsonreader = new JSONReader("C:\\Users\\phamm\\Downloads\\generated(1).json");
-		JSONtoXML jtox = new JSONtoXML(jsonreader.getJSONObjects());
-		System.out.println(jtox.jsontoxml());*/
-	}
+            /*JSONReader jsonreader = new JSONReader("C:\\Users\\phamm\\Downloads\\generated(1).json");
+            JSONtoXML jtox = new JSONtoXML(jsonreader.getJSONObjects());
+            System.out.println(jtox.jsontoxml());*/
+        }
 }
